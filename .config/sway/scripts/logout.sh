347b0b01aea0400c6e4a81f1f29915a57a025f55
juan <juancloudcomputing@protonmail.com>
2021-11-03 21:05:39 +0800

    updated cursor and lock script and zsh prompt

--- .config/sway/scripts/logout.sh
+++ .config/sway/scripts/logout.sh
@@ -1,18 +1,15 @@
 #!/bin/sh
 
-choice=`echo "shutdown\npoweroff\nreboot\nlock" | wofi -d -Oalphabetical`
+choice=`echo "suspend\nshutdown\npoweroff\nreboot\nlock" | wofi -d -Oalphabetical`
 if test "$choice" = lock ; then
-  tmp=/tmp/lockscreen.bmp
-  grim - | magick - -blur 0x6 $tmp
   bright=`light`
   swayidle -w \
     timeout 5 "light -S 1" resume "light -S $bright" \
     timeout 10 'swaymsg "output * dpms off"' resume 'swaymsg "output * dpms on"' \
     &
   idlepid=$!
-  swaylock -e -i $tmp -c 000000 --inside-color 00000003
+  swaylock -e -c 161821 --inside-color 00000003
   kill $idlepid
-  rm $tmp
   swaymsg "output * dpms on"
   light -S $bright
   return 0
