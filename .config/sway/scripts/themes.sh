daa5109e35a0dc7b49a37dd15493df91b7649a15
Ryan <ryan@alien.gov>
2024-12-30 18:34:52 +0800

    Use links for the theme colors

--- .config/sway/scripts/themes.sh
+++ .config/sway/scripts/themes.sh
@@ -22,30 +22,30 @@ if [ $scheme = "light" ]; then
     # kitty config
     kitty -1 sh -c "kitty @ set-colors -a -c ~/.config/kitty/themes/light.conf" &
     # fuzzel
-    cp ~/.config/fuzzel/fuzzel-light.ini ~/.config/fuzzel/fuzzel.ini
+    ln -sf ~/.config/fuzzel/fuzzel-light.ini ~/.config/fuzzel/fuzzel.ini
     # zathura
-    cp ~/.config/zathura/zathurarc.light ~/.config/zathura/zathurarc
+    ln -sf ~/.config/zathura/zathurarc.light ~/.config/zathura/zathurarc
     # qt5ct and qt6ct
-    cp ~/.config/qt5ct/light.conf ~/.config/qt5ct/qt5ct.conf
-    cp ~/.config/qt6ct/light.conf ~/.config/qt6ct/qt6ct.conf
+    ln -sf ~/.config/qt5ct/light.conf ~/.config/qt5ct/qt5ct.conf
+    ln -sf ~/.config/qt6ct/light.conf ~/.config/qt6ct/qt6ct.conf
     # gtk2
-    cp ~/.config/gtk-2.0/light.conf ~/.gtkrc-2.0
+    ln -sf ~/.config/gtk-2.0/light.conf ~/.gtkrc-2.0
     # gtk3
-    cp ~/.config/gtk-3.0/light.ini ~/.config/gtk-3.0/settings.ini
+    ln -sf ~/.config/gtk-3.0/light.ini ~/.config/gtk-3.0/settings.ini
 else
     # kitty config
     kitty -1 sh -c "kitty @ set-colors -a -c ~/.config/kitty/themes/dark.conf" &
     # zathura
-    cp ~/.config/zathura/zathurarc.dark ~/.config/zathura/zathurarc
+    ln -sf ~/.config/zathura/zathurarc.dark ~/.config/zathura/zathurarc
     # fuzzel
-    cp ~/.config/fuzzel/fuzzel-dark.ini ~/.config/fuzzel/fuzzel.ini
+    ln -sf ~/.config/fuzzel/fuzzel-dark.ini ~/.config/fuzzel/fuzzel.ini
     # qt5ct and qt6ct
-    cp ~/.config/qt5ct/dark.conf ~/.config/qt5ct/qt5ct.conf
-    cp ~/.config/qt6ct/dark.conf ~/.config/qt6ct/qt6ct.conf
+    ln -sf ~/.config/qt5ct/dark.conf ~/.config/qt5ct/qt5ct.conf
+    ln -sf ~/.config/qt6ct/dark.conf ~/.config/qt6ct/qt6ct.conf
     # gtk2
-    cp ~/.config/gtk-2.0/dark.conf ~/.gtkrc-2.0
+    ln -sf ~/.config/gtk-2.0/dark.conf ~/.gtkrc-2.0
     # gtk3
-    cp ~/.config/gtk-3.0/dark.ini ~/.config/gtk-3.0/settings.ini
+    ln -sf ~/.config/gtk-3.0/dark.ini ~/.config/gtk-3.0/settings.ini
 fi
 
 swaymsg reload
