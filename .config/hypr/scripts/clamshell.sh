a950a0b97f5edd2b1ae9333ce349e1afd3c8f910
Ryan <ryan@alien.gov>
2025-04-13 13:22:46 +0800

    Clean up hyprland
    I haven't used hyprland for a long while now, and the configuration may
    not be even usable.

--- .config/hypr/scripts/clamshell.sh
+++
@@ -1,6 +0,0 @@
-if hyprctl monitors | grep "Lectron Company Ltd LECOO M2412HL GG30A137" &>/dev/null
-then
-    hyprctl keyword monitor "eDP-1, disable"
-else
-    hyprctl keyword monitor "eDP-1, 2560x1440@90, auto, auto"
-fi
